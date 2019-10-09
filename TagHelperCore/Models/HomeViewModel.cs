65d412087449ed159c4b9c748292c78761a91c35
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-09 13:12:06 -0400

    Initial commit

---
+++ TagHelperCore/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TagHelperCore.Models
+{
+    public class HomeViewModel
+    {
+        public ICollection<SelectListItem> SelectedItems { get; set; }
+    }
+}
